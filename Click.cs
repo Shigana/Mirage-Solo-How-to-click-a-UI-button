7bd46b9d75388c9706234902f18f902a60836b8a
Shigana <76201160+Shigana@users.noreply.github.com>
2020-12-19 19:39:34 +0900

    Add files via upload

---
+++ Click.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Click : MonoBehaviour
+{
+    public Button target;
+    public int click;
+
+    private void Start()
+    {
+        target.onClick.AddListener(() => Debug.Log("click"));
+    }
+    private void Update()
+    {
+        if (click>0)
+        {
+            ExecuteEvents.Execute
+            (
+                target: target.gameObject,
+                eventData: new PointerEventData(EventSystem.current),
+                functor: ExecuteEvents.pointerClickHandler
+            );
+        }
+    }
+}
---
+++ CtrlInput.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CtrlInput : MonoBehaviour
+{
+    public StartClick start;
+    public StopClick stop;
+
+    GvrControllerInputDevice Controller;
+
+
+    void Start()
+    {
+        Controller = GvrControllerInput.GetDevice(GvrControllerHand.Dominant);
+    }
+    private float quitTime = 0;
+    private bool mirastart = true;
+
+    public void Update()
+    {
+
+        if (Controller.GetButtonDown(GvrControllerButton.TouchPadButton))
+        {
+
+            if (Controller.TouchPos.x < Controller.TouchPos.y & -Controller.TouchPos.x < Controller.TouchPos.y)
+            {
+                //Upper part of touchpad
+
+                Debug.Log("Top");
+            }
+            if (Controller.TouchPos.x > Controller.TouchPos.y & -Controller.TouchPos.x > Controller.TouchPos.y)
+            {
+                //Lower part of touchpad
+
+                Debug.Log("Bottom");
+            }
+            else if (Controller.TouchPos.x >= Controller.TouchPos.y & -Controller.TouchPos.x <= Controller.TouchPos.y)
+            {
+                click.GetComponent<Click>().click = 1;  //Right part of the touchpad
+            }
+
+            else if (Controller.TouchPos.x <= Controller.TouchPos.y & -Controller.TouchPos.x >= Controller.TouchPos.y)
+            {
+                click.GetComponent<Click>().click = 0;  //Left part of the touchpad
+            }
+        }
+        //ppos.GetComponent<PolePos>().plps = GetComponent<Slider>().value / 10;
+        //ptxt.GetComponent<plpsTxt>().plpstxt = GetComponent<Slider>().value / 10;
+    }
+
+    internal object Getcomponet<T>()
+    {
+        throw new NotImplementedException();
+    }
+}
